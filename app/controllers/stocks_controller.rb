29314b50c045a515c06d5e53bfcee112fa00a466
tahirfarid91 <tahirfarid91@hotmail.com>
2022-08-31 16:59:01 +0500

    flash messages

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,21 @@
 class StocksController < ApplicationController
 
+
     def search
-        stock = Stock.new_lookup(params[:stock])
-        render json: stock
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock])
+            if @stock
+                flash[:alert] = "The requested data is not available to free tier accounts. Please upgrade for access to this data."
+                redirect_to my_portfolio_path
+                #render 'users/my_portfolio'
+            else
+                flash[:alert] = "Please enter a valid symbol to search"
+                redirect_to my_portfolio_path
+            end
+        else
+            flash[:alert] = "Please enter a symbol to search"
+            redirect_to my_portfolio_path
+        end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
  
   devise_for :users
+  root 'welcome#index'
       devise_scope :user do  
       get '/users/sign_out' => 'devise/sessions#destroy'     
     end
-   root 'welcome#index'
+   
    get 'my_portfolio', to: 'users#my_portfolio'
    get 'search_stock', to: 'stocks#search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
