90dce3a1e4de976bc2c179f7e4313028684bca63
tahirfarid91 <tahirfarid91@hotmail.com>
2022-09-02 16:55:10 +0500

    begin/rescue

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,21 +1,27 @@
 class StocksController < ApplicationController
 
+	
 
-    def search
-        if params[:stock].present?
-            @stock = Stock.new_lookup(params[:stock])
-            if @stock
-                flash[:alert] = "The requested data is not available to free tier accounts. Please upgrade for access to this data."
-                redirect_to my_portfolio_path
-                #render 'users/my_portfolio'
-            else
-                flash[:alert] = "Please enter a valid symbol to search"
-                redirect_to my_portfolio_path
-            end
-        else
+  	def search
+        if	params[:stock].present?
+        	@stock = Stock.new_lookup(params[:stock])
+        
+			#if  params[:stock] == @stock[:ticker]
+
+				if 	@stock[:success]
+					flash[:alert] = "Success"
+					redirect_to my_portfolio_path
+				
+				else
+					flash[:alert] = @stock[:message]
+					redirect_to my_portfolio_path
+				end
+							
+      	else
             flash[:alert] = "Please enter a symbol to search"
             redirect_to my_portfolio_path
         end
-    end
+  	end
 
 end
+  
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,14 +1,22 @@
 class Stock < ApplicationRecord
+  
 
+  
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: 
                                     Rails.application.credentials.iex_client[:iex_api_key],
                                     secret_token: 'sk_7d4983a3f6a54fd08d1dff51b82be876',
                                     endpoint: 'https://cloud.iexapis.com/v1')
       begin
-        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-      rescue => exception
-        return nil
+        respnose = new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol)) 
+        {success: true, data: response, message: "Successfully Fetched!!!"}
+      rescue => e
+         {success: false, data: nil, message: e.error}
+    
+              
+      
+      end
     end
-  end
+
+    
 end
