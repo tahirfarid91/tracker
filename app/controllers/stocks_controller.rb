0266ad4ea6dd0b347705f5a80d3d29c49e2fe91b
tahirfarid91 <tahirfarid91@hotmail.com>
2022-09-15 11:36:23 +0500

    Displaying Errors to users

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,13 @@
 class StocksController < ApplicationController
 
-	
 
   	def search
         if	params[:stock].present?
         	@stock = Stock.new_lookup(params[:stock])
-        
-			#if  params[:stock] != @stock[:ticker]
-
+    
 				if 	@stock[:success]
 					flash[:alert] = "Success"
-					redirect_to my_portfolio_path
-
-				
+					redirect_to my_portfolio_path	
 				else
 					flash[:alert] = @stock[:message]
 					redirect_to my_portfolio_path
@@ -23,6 +18,7 @@ class StocksController < ApplicationController
             redirect_to my_portfolio_path
         end
   	end
+
 
 end
   
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,24 +1,23 @@
 class Stock < ApplicationRecord
- has_many :user_stocks
- has_many :users, through: :user_stocks
+   has_many :user_stocks
+   has_many :users, through: :user_stocks
 
 
- #validates :name, ticker, presence: true
-
-  
+ 
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: 
                                     Rails.application.credentials.iex_client[:iex_api_key],
                                     secret_token: 'sk_7d4983a3f6a54fd08d1dff51b82be876',
                                     endpoint: 'https://cloud.iexapis.com/v1')
-      begin
-        respnose = new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol)) 
-        {success: true, data: response, message: "Successfully Fetched!!!"}
-      rescue => e
-        {success: false, data: nil, message: e.error}
-  
-      end
+        begin
+          respnose = new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol)) 
+          {success: true, data: response, message: "Successfully Fetched!!!"}
+        rescue => e
+          {success: false, data: nil, message: e}
+
+        end
     end
 
     
+
 end
